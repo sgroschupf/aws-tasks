621208deaede6e7a3405f88cad683a9e7f63e43e
Johannes Zillmann <jz@datameer.com>
2010-02-25 20:09:10 +0100

    add license header

--- src/main/java/datameer/awstasks/aws/emr/EmrCluster.java
+++ src/main/java/datameer/awstasks/aws/emr/EmrCluster.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package datameer.awstasks.aws.emr;
 
 import java.io.File;
@@ -264,23 +279,6 @@ public class EmrCluster {
                 Thread.sleep(requestInterval);
             }
         } while (!finished);
-    }
-
-    private <T> T doRateExceededAwareRequest(int requestInterval, Callable<T> callable) throws InterruptedException, AmazonElasticMapReduceException {
-        Thread.sleep(requestInterval);
-        try {
-            return callable.call();
-        } catch (AmazonElasticMapReduceException e) {
-            if (e.getErrorCode().equals("Throttling")) {
-                return doRateExceededAwareRequest(requestInterval, callable);
-            }
-            throw e;
-        } catch (Exception e) {
-            if (e instanceof RuntimeException) {
-                throw (RuntimeException) e;
-            }
-            throw new RuntimeException(e);
-        }
     }
 
     protected JobFlowDetail getJobFlowDetail(String jobFlowId) throws AmazonElasticMapReduceException {
